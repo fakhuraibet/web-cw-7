64994debe505c2ce87be77a8b30145d25515b37c
fakhuraibet <farah.kh120@gmail.com>
2022-03-26 01:48:16 +0300

    done CW1 2 and 3 with no bonus
    I will add bonuses tomorrow.

--- Random Password/script.js
+++ Random Password/script.js
@@ -1,3 +1,92 @@
-// Database قاعدة البيانات
+const chars = [
+  {
+    1: "abcdefghijklmnopqrstuvwxyz",
+  },
 
-// دالة generate
+  {
+    2: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  },
+
+  {
+    3: "0123456789",
+  },
+
+  {
+    4: "!@#$%^&*()_+~}{[]:;?.><,./-=",
+  },
+];
+
+function generate() {
+  let randomPassword = "";
+
+  let collection_one = chars[0]["1"];
+  let collection_two = chars[1]["2"];
+  let collection_three = chars[2]["3"];
+  let collection_four = chars[3]["4"];
+
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_three.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_three.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_three.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_three.charAt(Math.floor(Math.random() * collection_one.length));
+
+  randomPassword =
+    randomPassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_one.length));
+
+  display.value = randomPassword;
+}
--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,21 @@
-// project أنشئ كائن
+const project = {
+  type: "tech",
+  users: 1000,
+  biography() {
+    return "This is my project";
+  },
+};
 
-// randomProperty أنشئ دالة
+function randomProperty(obj) {
+  const keys = Object.keys(obj);
+
+  const random = Math.floor(Math.random() * keys.length);
+
+  //console.log(keys[`${random}`]);
+  //console.log(obj[` ${keys[`${random}`]} `]);
+
+  console.log(keys[random]);
+  console.log(obj[`${keys[random]}`]);
+}
+
+randomProperty(project);
